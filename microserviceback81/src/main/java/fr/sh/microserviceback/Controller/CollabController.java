dcdd590b770eba481194fa832b2dc83a761fdee1
lglimois <lglimois@gmail.com>
2019-03-09 12:02:51 +0100

    log rotate

--- microserviceback81/src/main/java/fr/sh/microserviceback/Controller/CollabController.java
+++ microserviceback81/src/main/java/fr/sh/microserviceback/Controller/CollabController.java
@@ -3,6 +3,8 @@ package fr.sh.microserviceback.Controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,9 +15,12 @@ import fr.sh.microserviceback.dto.Collaborator;
 @RestController
 public class CollabController {
 
+	Logger logger = LoggerFactory.getLogger(CollabController.class);
+	
 	@RequestMapping("/api/collaborator")
 	public List getCollab() {
-
+		logger.info("Appel: /api/collaborator");
+		
 		List list = new ArrayList<>();
 		for (int i = 1; i < 3; i++) {
 			Collaborator collaborator = new Collaborator("firstname"+i, "lastname"+i, "email"+i);
